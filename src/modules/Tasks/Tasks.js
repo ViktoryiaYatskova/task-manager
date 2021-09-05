2059e304bf93438d2a108da6b8ddd8b969509d32
Viktoria Khaslarova <v_khaslarova@flo.health>
2021-09-05 20:53:36 +0300

    #1: add saga for initial tasks fetch

--- src/modules/Tasks/Tasks.js
+++ src/modules/Tasks/Tasks.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Button } from 'components/atoms'
 import { TaskList } from 'components/organisms';
+import { tasksListSelector } from '../../reducers/tasksReducer/selectors'
 
-const Tasks = () => (
-  <div>
-    <h1>Processes</h1>
-    <TaskList />
-    <Button>Create</Button>
-  </div>
-)
+const Tasks = () => {
+  const tasks = useSelector(tasksListSelector);
+
+  return (
+    <div>
+      <h1>Processes</h1>
+      <TaskList tasks={tasks} />
+      <Button>Create</Button>
+    </div>
+  )
+}
 
 export default Tasks
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,12 +1,14 @@
 import { createBrowserHistory } from 'history'
 import { combineReducers } from '@reduxjs/toolkit'
 import { connectRouter } from 'connected-react-router'
+import tasksReducer from 'reducers/tasksReducer/tasksReducer';
 
 const history = createBrowserHistory();
 
 const reducer = combineReducers({
   router: connectRouter(history),
-})
+  tasks: tasksReducer,
+});
 
 export { history }
 
