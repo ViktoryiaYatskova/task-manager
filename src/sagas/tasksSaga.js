9bd296fc78b6b392b543e02331c57236ad8950e0
Viktoria Khaslarova <v_khaslarova@flo.health>
2021-09-05 21:29:11 +0300

    #10: use createAction, createReducer

--- src/sagas/tasksSaga.js
+++ src/sagas/tasksSaga.js
@@ -1,12 +1,13 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
 import fetchTasks from '../api/tasks/fetchTasks';
+import { tasksFetchAction, tasksSet } from '../reducers/tasksReducer/actions'
 
 export function* fetchTasksSaga() {
   const tasks = yield call(fetchTasks);
 
-  yield put({ type: 'tasks/set', payload: tasks });
+  yield put(tasksSet(tasks));
 }
 
 export function* watchFetchTasks() {
-  yield takeEvery('tasks/fetch', fetchTasksSaga)
+  yield takeEvery(tasksFetchAction, fetchTasksSaga)
 }
