29961312a53a0d58f6deeadc22f87d5b1063dc09
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-07 00:46:33 +0300

    Prettify styles for the list wrapper

--- src/components/templates/Default/Default.js
+++ src/components/templates/Default/Default.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BorderedCenterArea } from './Default.styles';
 
-const Default = ({ children }) => <div>{children}</div>
+const Default = ({ children }) => <BorderedCenterArea>{children}</BorderedCenterArea>;
 
 Default.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Default
+export default Default;
