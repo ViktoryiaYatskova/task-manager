d65007a9f818fae26d5bbbd3f8165644c55c8624
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-08 08:35:30 +0000

    Refactor: move subtasks selector into SubTasksList

--- src/components/organisms/Task/Task.js
+++ src/components/organisms/Task/Task.js
@@ -1,23 +1,21 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Button } from 'components/atoms';
 import { subTasksFetchAction } from 'reducers/tasksReducer/actions';
 import SubTasksList from 'components/organisms/SubTasksList/SubTasksList';
-import { subTasksSelector } from 'reducers/tasksReducer/selectors';
 
 const Task = ({ title, id }) => {
   const dispatch = useDispatch();
   const onFetchSubtasks = useCallback(() => {
     dispatch(subTasksFetchAction(id));
   }, [id, dispatch]);
-  const subTasks = useSelector(state => subTasksSelector(state, id));
 
   return (
     <>
       <span>{title}</span>
       <Button onClick={onFetchSubtasks}>View Subtasks</Button>
-      <SubTasksList subTasks={subTasks} />
+      <SubTasksList taskId={id} />
     </>
   );
 };
