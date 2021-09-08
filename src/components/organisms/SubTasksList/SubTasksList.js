4e0a37bdfdb84186f4317a4f9b4423a07bf66af7
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-08 12:58:35 +0000

    Refactor: notify about success delete before sending sucess-event

--- src/components/organisms/SubTasksList/SubTasksList.js
+++ src/components/organisms/SubTasksList/SubTasksList.js
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import SubTask from 'components/molecules/SubTask/SubTask';
 import { subTasksSelector } from 'reducers/tasksReducer/selectors';
+import { List } from './SubTasksList.styles';
 
 const SubTasksList = ({ taskId }) => {
   const safeSubTasksSelector = useCallback(state => subTasksSelector(state, taskId), [taskId]);
   const subTasks = useSelector(safeSubTasksSelector);
 
   return (
-    <ul>
+    <List>
       {subTasks.map(subTask => (
         <li key={subTask.id}>
           <SubTask {...subTask} />
         </li>
       ))}
-    </ul>
+    </List>
   );
 };
 
--- src/components/organisms/TaskList/TaskList.styles.js
+++ src/components/organisms/TaskList/TaskList.styles.js
@@ -1,3 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  margin-bottom: 5px;
+
+  .task {
+    /* styles specific for tasks belonged to the list */
+    margin-bottom: 5px;
+  }
+`;
