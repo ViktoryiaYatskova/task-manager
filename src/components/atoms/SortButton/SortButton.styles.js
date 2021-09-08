ffee5e34b38b2e2941ff0f1ee2d09ac9d4ef13fd
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-08 22:57:47 +0300

    Refactor: remove esline-ignore + add useCallback usage note

---
+++ src/components/atoms/SortButton/SortButton.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Button } from 'components/atoms';
+
+export const SortButton = styled(Button)`
+  & {
+    background-color: #ffe082;
+    margin-right: 5px;
+  }
+`;
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/Button';
+export { default as SortButton } from './SortButton/SortButton';
 export { default as TextInput } from './TextInput/TextInput';
 export { default as PageHeader } from './PageHeader/PageHeader';
 export { default as List } from './List/List';
