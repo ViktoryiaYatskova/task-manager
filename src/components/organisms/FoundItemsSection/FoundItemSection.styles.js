e8b9f3d785f953f7550c3bff56bd152cf5ec8e57
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-09 22:48:46 +0300

    Refactor: minor style code + remove redundand found section

--- src/components/organisms/FoundItemsSection/FoundItemSection.styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const FoundSection = styled.section`
-  margin: 10px 0;
-`;
--- src/modules/Tasks/Tasks.js
+++ src/modules/Tasks/Tasks.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { TaskList } from 'components/organisms';
 import PageHeader from 'components/atoms/PageHeader/PageHeader';
 import TaskForm from 'components/organisms/TaskForm/TaskForm';
-import FoundItemsSection from 'components/organisms/FoundItemsSection/FoundItemsSection';
 
 const TasksPage = () => (
   <>
     <PageHeader />
     <main>
       <TaskForm />
-      <FoundItemsSection />
       <TaskList />
     </main>
   </>
