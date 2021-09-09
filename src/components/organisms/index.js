033e3acee6aa0f059a4683d7680d77bb23b677ff
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-09 22:48:46 +0300

    Refactor: minor style code + remove redundand found section

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,4 +1,2 @@
 export { default as TaskList } from './TaskList/TaskList';
-export { default as FoundSubTasksList } from './FoundSubTasksList/FoundSubTasksList';
-export { default as FoundTaskList } from './FoundTaskList/FoundTaskList';
 export { default as Task } from './Task/Task';
