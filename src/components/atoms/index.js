1dec3e7b53a756380a9bb29c03069236630d9ec2
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-10 10:01:54 +0300

    Refactor: clean-up unused entities

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,6 +3,5 @@ export { default as SortButton } from './SortButton/SortButton';
 export { default as TextInput } from './TextInput/TextInput';
 export { default as PageHeader } from './PageHeader/PageHeader';
 export { default as List } from './List/List';
-export { default as FoundTask } from './FoundTask/FoundTask';
 export { default as Checkbox } from './Checkbox/Checkbox';
 export { default as MarkedText } from './MarkedText/MarkedText';
