d4cb7098018b5e91f392777fbb6e0a58c793efbd
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-10 11:24:20 +0000

    Updates

---
+++ static/js/main.ea2a61d7.chunk.js
@@ -0,0 +1,65 @@
+(this["webpackJsonporca-test-task"]=this["webpackJsonporca-test-task"]||[]).push([[0],{1556:function(e,t,s){"use strict";s.r(t);var a=s(0),c=s.n(a),r=s(21),i=s.n(r),n=s(11),o=s(20),l=s(8),b=s(45),d=s(51);const j=window.alert;var u=(...e)=>{j(e.join(" "))},O=s(14),h=s(17);const p=Object(l.b)("tasks/set"),y=Object(l.b)("task/create"),k=Object(l.b)("tasks/fetch"),f=Object(l.b)("tasks/set/found"),m=Object(l.b)("tasks/reset/found");var x=Object(l.c)({tasksList:[],foundTasksList:[]},(e=>{e.addCase(p,((e,{payload:t})=>({...e,tasksList:t}))).addCase(f,((e,{payload:t})=>({...e,foundTasksList:t}))).addCase(m,(e=>({...e,foundTasksList:[]})))}));const g=Object(l.b)("app/mode"),C=Object(l.b)("app/searchItems"),T=Object(l.b)("subTasks/fetch"),v=Object(l.b)("subTasks/set"),w=Object(l.b)("subTasks/set/found"),S=Object(l.b)("subTask/reset/found"),I=Object(l.b)("subTask/delete"),L=Object(l.b)("subTask/delete/success"),E=Object(l.b)("subTask/search:byLabel");var N=Object(l.c)({allSubTasks:{},foundSubTasksList:[],filters:[]},(e=>{e.addCase(v,((e,{payload:t})=>({...e,allSubTasks:{...e.allSubTasks,[t.taskId]:t.subTasks}}))).addCase(w,((e,{payload:t})=>({...e,foundSubTasksList:t}))).addCase(S,(e=>({...e,foundSubTasksList:[]}))).addCase(E,((e,{payload:t})=>({...e,filters:[...t]}))).addCase(g,((e,{payload:t})=>({...e,filters:t?[]:e.filters})))}));var B=Object(l.c)({isSearchMode:!1,searchQuery:""},(e=>{e.addCase(E,(e=>({...e,isSearchMode:!0,searchQuery:""}))).addCase(g,((e,{payload:t})=>({...e,isSearchMode:t,searchQuery:t?e.searchQuery:""}))).addCase(C,((e,{payload:t})=>({...e,searchQuery:t})))}));const M=Object(O.a)();var R=Object(h.b)({router:Object(o.b)(M),tasks:x,subTasks:N,app:B}),A=s(7),P=s(19),q=s.n(P);class _{constructor(e){this.key=null,this.key=e,localStorage.getItem(this.key)||localStorage.setItem(this.key,JSON.stringify([]))}get(){try{return JSON.parse(localStorage.getItem(this.key))}catch{return null}}set(e){localStorage.setItem(this.key,JSON.stringify(e))}}var F={tasks:new _("tasks"),subTasks:new _("subTasks")},Q=e=>(...t)=>new Promise((s=>setTimeout((()=>s(e(...t))),1500)));const D=Array.from({length:10}).map((()=>q.a.lorem.word())),J=(e,t)=>Math.floor(Math.random()*(t-e+1))+e;var $=Q((({title:e})=>{const t=F.tasks.get(),s=F.subTasks.get(),a={id:q.a.datatype.uuid(),createTime:Date.now(),title:e||q.a.lorem.sentence()},c=Array.from({length:J(1,10)}).map((()=>({id:q.a.datatype.uuid(),taskId:a.id,title:q.a.lorem.sentence(),labels:[...new Set(Array.from({length:J(1,5)}).map((()=>D[J(0,9)])))]})));return F.tasks.set([...t,a]),F.subTasks.set([...s,...c]),a})),z=Q((()=>F.tasks.get())),U=Q((e=>{const t=F.tasks.get(),s=F.tasks.get().find((t=>t.id===e));if(!s)throw new Error(`Task ${e} wasn't found`);return F.tasks.set([...t.filter((t=>t.id!==e))]),s})),V=Q((e=>{const t=F.tasks.get(),s=e.toLowerCase();return t.filter((e=>e.title.toLowerCase().includes(s)))})),W=s(23);const Y=e=>e.app.isSearchMode,G=e=>e.app.searchQuery,H=e=>e.subTasks.foundSubTasksList,K=e=>e.subTasks.filters,X=(e,t)=>e.subTasks.allSubTasks[t]||[],Z=e=>Object(W.a)(Y,(e=>Object(W.a)(H,(t=>t.filter((t=>t.taskId===e)))))(e),(t=>X(t,e)),((e,t,s)=>e?t:s));function*ee(){const e=yield Object(A.b)(z);yield Object(A.c)(p(e))}function*te({payload:e}){const t=yield Object(A.b)(V,e);yield Object(A.c)(f(t))}function*se({payload:e}){yield Object(A.b)($,e),yield Object(A.b)(ee)}function*ae({payload:e}){try{yield Object(A.b)(U,e),yield Object(A.b)(ee)}catch(t){u("Delete task failed:",t)}}function*ce({payload:e}){const{taskId:t,id:s}=e;((e,t)=>!t.length||1===t.length&&t.some((({id:t})=>t===e)))(s,yield Object(A.d)(X,t))&&(yield Object(A.b)(ae,{payload:t}))}function*re(){yield Object(A.f)(k.type,ee),yield Object(A.e)(y.type,se),yield Object(A.f)(C.type,te),yield Object(A.e)(L.type,ce)}var ie=Q((e=>{const t=F.subTasks.get(),s=t.find((t=>t.id===e));if(s)return F.subTasks.set(t.filter((t=>t.id!==e))),s;throw new Error(`SubTask [${e}] not found`)})),ne=Q((e=>F.subTasks.get().filter((t=>t.taskId===e)))),oe=Q((e=>{const t=F.subTasks.get(),s=e.toLowerCase();return t.filter((e=>e.title.toLowerCase().includes(s)))})),le=Q((e=>{const t=F.subTasks.get();return e.reduce(((e,t)=>{const s=t.toLowerCase();return e.filter((e=>e.labels.some((e=>e.toLowerCase()===s))))}),t)}));function*be({payload:e}){const t=yield Object(A.b)(ne,e);yield Object(A.c)(v({taskId:e,subTasks:t}))}function*de({payload:e}){const t=yield Object(A.b)(oe,e);yield Object(A.c)(w(t))}function*je({payload:e}){const t=yield Object(A.b)(le,e);yield Object(A.c)(w(t))}function*ue({payload:e}){try{const{id:t,taskId:s}=e;yield Object(A.b)(ie,t),yield Object(A.c)(L(e)),yield Object(A.b)(be,{payload:s})}catch(t){u("Delete subTask request failed:",t)}}function*Oe(){yield Object(A.e)(T.type,be),yield Object(A.e)(I.type,ue),yield Object(A.f)(C.type,de),yield Object(A.f)(E.type,je)}function*he(){yield Object(A.b)(ee)}const pe=Object(b.a)(M),ye=Object(d.a)({onError:(e,{sagaStack:t})=>{u("Something came up",e,t)}}),ke=[...Object(l.d)({thunk:!1,serializableCheck:!1,immutableCheck:{warnAfter:300}}),pe,ye],fe=Object(l.a)({reducer:R,middleware:ke,enhancers:[],preloadedState:{},devTools:{trace:!0}});ye.run((function*(){yield Object(A.f)("app/init",he),yield Object(A.a)([re(),Oe()])}));var me=fe,xe=s(9);const ge=()=>"/";var Ce=s(10);const Te=Ce.b.div.withConfig({displayName:"BorderedCenterArea"})(["width:500px;margin:20px auto;padding:20px;max-width:100%;border:1px dashed #a5a2a2;"]);var ve=s(2);var we=({children:e})=>Object(ve.jsx)(Te,{children:e}),Se=s(5),Ie=s.n(Se);const Le=Ce.b.button.withConfig({displayName:"Wrapper"})(["padding:4px 6px;height:fit-content;border-radius:1em;background-color:#96fbe4;border:none;&.yellow-button{background-color:#ffe082;}&.purple-button{background-color:#9c9cc7;color:#fff;}"]);var Ee=e=>Object(ve.jsx)(Le,{...e});const Ne={CREATE_TIME:"BY_CREATE_TIME",TITLE:"BY_TITLE"},Be=Object(Ce.b)(Ee).withConfig({displayName:"SortButton"})(["margin-right:5px;"]),Me={[Ne.CREATE_TIME]:"Sort by create-date",[Ne.TITLE]:"Sort by title"};var Re=({sortType:e,sortBy:t})=>{const s=Object(a.useCallback)((()=>t(e)),[t,e]);return Object(ve.jsx)(Be,{className:"yellow-button",onClick:s,children:Me[e]})};const Ae=Ce.b.input.attrs({type:"text"}).withConfig({displayName:"Input"})(["border-radius:1em;border:1px solid #656565;padding:2px 5px;"]),Pe=({value:e,placeholder:t,onValueChange:s})=>{const[c,r]=Object(a.useState)(e),i=Object(a.useCallback)((e=>{r(e.target.value),s(e.target.value)}),[s]);return Object(a.useEffect)((()=>r(e)),[e]),Object(ve.jsx)(Ae,{placeholder:t,value:c,onChange:i})};Pe.defaultProps={value:"",placeholder:""};var qe=Pe;var _e=()=>Object(ve.jsx)("h1",{children:"Processes"});const Fe=Ce.b.ul.withConfig({displayName:"ListContainter"})(["margin-left:10px;margin-bottom:5px;.item{margin-bottom:5px;}"]);var Qe=({items:e,ItemComponent:t})=>Object(ve.jsx)(Fe,{children:e.map((e=>Object(ve.jsx)("li",{children:Object(ve.jsx)(t,{...e})},e.id)))});const De=Ce.b.input.attrs({type:"checkbox"}).withConfig({displayName:"Checkbox"})(["margin-right:5px;"]),Je=({onCheck:e,isUncheckable:t})=>{const[s,c]=Object(a.useState)(!1),r=Object(a.useCallback)((()=>{s||e(),c((e=>!!t||!e))}),[e,s,t]);return Object(ve.jsx)(De,{onChange:r,checked:s})};Je.defaultProps={isUncheckable:!1};var $e=Je;var ze=({children:e})=>{const t=Object(n.e)(G),s=new RegExp(`(${t})`,"gi"),a=e.split(s);return t?Object(ve.jsx)(ve.Fragment,{children:a.map(((e,t)=>{const a=s.test(e);return Object(ve.jsx)(c.a.Fragment,{children:a?Object(ve.jsx)("mark",{children:e}):e},t)}))}):e};const Ue=Ce.b.div.withConfig({displayName:"ButtonContainer"})(["margin:5px 0;"]);var Ve=({sortBy:e})=>Object(ve.jsx)(Ue,{children:Object.values(Ne).map((t=>Object(ve.jsx)(Re,{sortBy:e,sortType:t},t)))});const We=Object(Ce.b)(Ee).withConfig({displayName:"LabelStyled"})(["padding:0 5px;"]),Ye=Ce.b.ul.withConfig({displayName:"LabelsListContainer"})(["margin:3px 0 10px;& > li{display:inline-block;margin-right:5px;}"]),Ge=Ce.b.li.withConfig({displayName:"LabelListItem"})(["display:inline-block;margin-right:5px;"]),He=({label:e})=>{const t=Object(n.e)(K),s=Object(n.d)(),c=Object(a.useCallback)((()=>s(E([...t,e]))),[s,E,e]);return Object(ve.jsxs)(We,{className:"label purple-button",onClick:c,children:["#",e]})},Ke=({labels:e})=>Object(ve.jsx)(Ye,{children:e.map((e=>Object(ve.jsx)(Ge,{children:Object(ve.jsx)(He,{label:e})},e)))});Ke.defaultProps={labels:[]};var Xe=Ke;const Ze=({id:e,title:t,labels:s,taskId:c})=>{const r=Object(n.d)(),i=Object(a.useCallback)((()=>r(I({id:e,taskId:c}))),[r,e,c]);return Object(ve.jsxs)("span",{children:[Object(ve.jsx)($e,{onCheck:i,isUncheckable:!0}),Object(ve.jsx)("span",{id:e,children:Object(ve.jsx)(ze,{children:t})}),Object(ve.jsx)(Xe,{labels:s})]})};Ze.defaultProps={labels:[]};Ie.a.string.isRequired,Ie.a.string.isRequired,Ie.a.string.isRequired,Ie.a.arrayOf(Ie.a.string);var et=Ze;var tt=({taskId:e})=>{const t=((e,t)=>{const s=Object(a.useCallback)(e(...t),[e,...t]);return Object(n.e)(s)})(Z,[e]);return Object(ve.jsx)(Qe,{items:t,ItemComponent:et})};const st=e=>{const t=new Date(e);return`${t.toLocaleDateString()} ${t.toLocaleTimeString()}`},at=(e,t,s)=>e.filter((e=>(e=>{return t=e,a="id",s.find((e=>e[a]===t[a]));var t,a})(e.id)||((e,t)=>e.find((e=>e.taskId===t)))(t,e.id))),ct=Ce.b.div.withConfig({displayName:"TaskWrapper"})(["display:flex;justify-content:space-between;"]),rt=Ce.b.span.withConfig({displayName:"TaskName"})(["flex-basis:40%;max-width:40%;overflow:hidden;text-overflow:ellipsis;"]);Ie.a.string.isRequired,Ie.a.string.isRequired,Ie.a.number.isRequired;var it=({title:e,id:t,createTime:s})=>{const c=Object(n.d)(),r=Object(a.useCallback)((()=>c(T(t))),[t,c]);return Object(ve.jsxs)(ve.Fragment,{children:[Object(ve.jsxs)(ct,{children:[Object(ve.jsx)(rt,{children:Object(ve.jsx)(ze,{children:e})}),Object(ve.jsx)("span",{children:st(s)}),Object(ve.jsx)(Ee,{onClick:r,children:"Expand"})]}),Object(ve.jsx)(tt,{taskId:t})]})};const nt=Object(W.a)(Y,(e=>e.tasks.tasksList),(e=>e.tasks.foundTasksList),H,((e,t,s,a)=>e?at(t,a,s):t)),ot={[Ne.CREATE_TIME]:(e,t)=>e.createTime-t.createTime,[Ne.TITLE]:(e,t)=>e.title<t.title?-1:1};var lt=e=>{const[t,s]=Object(a.useState)(Ne.CREATE_TIME);return{sortedItems:((e,t)=>{const s=[...e];return s.sort(ot[t]),s})(e,t),sortBy:s}};var bt=()=>{const e=Object(n.e)(nt),{sortedItems:t,sortBy:s}=lt(e);return Object(ve.jsxs)("section",{children:[Object(ve.jsx)(Ve,{sortBy:s}),Object(ve.jsx)(Qe,{items:t,ItemComponent:it})]})};var dt=()=>{const e=Object(n.e)(K);return Object(ve.jsx)("div",{children:Object(ve.jsx)(Xe,{labels:e})})};const jt=Ce.b.section.withConfig({displayName:"TaskFormContainer"})(["& button{margin-left:5px;}"]),ut=({title:e})=>{const[t,s]=Object(a.useState)(e),c=Object(n.d)(),r=Object(n.e)(Y),i=Object(a.useCallback)((()=>{c(y({title:t})),s("")}),[t,c]),o=Object(a.useCallback)((()=>{r?s(""):c(C(t)),c(g(!r))}),[c,r,t]),l=Object(a.useCallback)((e=>{s(e),r&&c(C(e))}),[r,c]);return Object(ve.jsxs)(jt,{children:[Object(ve.jsx)(qe,{value:t,placeholder:"task title",onValueChange:l}),!r&&Object(ve.jsx)(Ee,{onClick:i,children:"Create"}),Object(ve.jsx)(Ee,{onClick:o,children:r?"Exit Search":"Start Search"}),Object(ve.jsx)(dt,{})]})};ut.defaultProps={title:""};var Ot=ut;var ht=()=>Object(ve.jsxs)(ve.Fragment,{children:[Object(ve.jsx)(_e,{}),Object(ve.jsxs)("main",{children:[Object(ve.jsx)(Ot,{}),Object(ve.jsx)(bt,{})]})]});var pt=()=>Object(ve.jsx)(we,{children:Object(ve.jsx)(ht,{})}),yt=s(26);var kt=()=>Object(ve.jsxs)(we,{children:[Object(ve.jsx)("h1",{children:"Page not found"}),Object(ve.jsx)(yt.a,{to:ge(),children:"Go to home"})]});var ft=()=>Object(ve.jsxs)(xe.c,{children:[Object(ve.jsx)(xe.a,{path:ge(),component:pt}),Object(ve.jsx)(xe.a,{component:kt})]}),mt=Ce.a`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: none;
+
+    &,
+    &:before,
+    &:after {
+      box-sizing: border-box;
+    }
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Helvetica', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.4;
+  }
+
+  a {
+    text-decoration: none;
+
+    &,
+    &:hover {
+      color: inherit;
+    }
+  }
+
+  p {
+    margin: 0;
+  }
+
+  ul,
+  ol {
+    margin: 0;
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+
+  pre {
+    margin: 0;
+  }
+
+  #root {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+`;var xt=()=>(Object(a.useEffect)((()=>{me.dispatch({type:"app/init"})}),[]),Object(ve.jsxs)(ve.Fragment,{children:[Object(ve.jsx)(mt,{}),Object(ve.jsx)(n.a,{store:me,children:Object(ve.jsx)(o.a,{history:M,children:Object(ve.jsx)(ft,{})})})]}));window.logError=alert,i.a.render(Object(ve.jsx)(c.a.StrictMode,{children:Object(ve.jsx)(xt,{})}),document.getElementById("root"))}},[[1556,1,2]]]);
+//# sourceMappingURL=main.ea2a61d7.chunk.js.map
