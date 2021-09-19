9265df9b3594d7b66a509a156623134ba14ae5a9
ViktoryiaYatskova <yatskovaVD@gmail.com>
2021-09-19 18:13:40 +0000

    refactor: remove not used helper

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,16 +1,5 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const mapOmitEmpty = (array, callback) =>
-  array.reduce((newArray, element, index) => {
-    const newElement = callback(element, index);
-
-    // filter null and undefined:
-    // eslint-disable-next-line eqeqeq
-    if (newElement != undefined) newArray.push(newElement);
-
-    return newArray;
-  }, []);
-
 export const logRequest =
   (requestName, func) =>
   (...args) => {
